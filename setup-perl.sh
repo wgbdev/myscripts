43980e78a34842f73dd5d6aef32e8c341304617a
Thing Two <thing-two@Things-iMac-2.local>
2019-02-16 11:52:50 -0800

    initial add

---
+++ setup-perl.sh
@@ -0,0 +1,15 @@
+
+
+# https://forumserver.twoplustwo.com/48/computer-technical-help/perl-parse-recdescent-install-319416/
+
+
+echo "perl -MCPAN -e shell"
+echo
+echo "cpan> o conf prerequisites_policy ask <enter>"
+echo
+echo "cpan> install Bundle::CPAN <enter>"
+echo "cpan> install Graph::Directed <enter>"
+echo "cpan> install Spreadsheet::WriteExcel <enter>"
+echo "cpan> quit <enter>"
+
+
