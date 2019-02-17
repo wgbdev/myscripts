b178cb31e3a2d0c660c15c36c5920d6fac30e486
W. Glen Boyd <glen@wgbdev.com>
2019-02-17 03:00:35 +0100

    error checking for parameters

--- slick.sh
+++ slick.sh
@@ -1,1 +1,10 @@
-find ${1} -type f -exec grep -I -q ${2} {} \; -print
+if [ $# -gt 1 ]
+then
+	find ${1} -type f -exec grep -I -q ${2} {} \; -print
+else
+	echo
+	echo	"Missing Parameters:"
+	echo
+	echo	"Format: ./slick.sh /path search-text"
+	echo
+fi
