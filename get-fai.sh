cf0757e145174b3286c68b8e553675fa57e3f5ad
W. Glen Boyd <glen@wgbdev.com>
2019-02-17 01:34:46 +0100

    added recursive back in

--- get-fai.sh
+++ get-fai.sh
@@ -1,1 +1,2 @@
-git clone --recursive https://github.com/wgbdev/fai.git ${1}
+git clone --recursive https://github.com/wgbdev/fai.git fai-build/fai
+#git clone https://github.com/wgbdev/fai.git fai-build/fai
