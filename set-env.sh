62b183d5d3f720a81fc99a69058abd60817a0065
W. Glen Boyd <glen@wgbdev.com>
2019-02-16 22:18:27 -0800

    refactored

--- set-env.sh
+++
@@ -1,5 +0,0 @@
-echo "first script"
-git config --global user.name "W. Glen Boyd"
-git config --global user.email "glen@wgbdev.com"
-
-#git config --global core.editor nano 
--- set-git-env.sh
+++ set-git-env.sh
@@ -1,4 +1,4 @@
-echo "first script"
+echo "First script to setup git..."
 git config --global user.name "W. Glen Boyd"
 git config --global user.email "glen@wgbdev.com"
 
