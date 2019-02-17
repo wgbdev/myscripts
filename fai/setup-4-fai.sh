b2a59419abc17b7d67e5141adbf558dedf1a031d
W. Glen Boyd <glen@wgbdev.com>
2019-02-17 09:13:29 +0100

    clean up the scripts

--- fai/setup-4-fai.sh
+++ fai/setup-4-fai.sh
@@ -8,15 +8,21 @@ apt-get install libgraph-perl -y
 apt-get install libparse-recdescent-perl -y
 apt-get install asciidoc -y
 apt-get install w3m -y
-# - Let`s hope we don`t need the following, it's really fucking big....
-#apt-get install dblatex -y
 apt-get install asciidoc-dblatex
 
-echo
-echo "-------------------------------------------------------"
-echo "NOTE:"
-echo "-------------------------------------------------------"
-echo "Now call ./setup-perl.sh to install the needed packages"
-echo
+#
+# - Let`s hope we don`t need the following, it's really fucking big....
+# --- so far it doesn't appear to be needed....
+# apt-get install dblatex -y
+#
 
+echo
+echo "----------------------------------------------------------"
+echo
+echo "All updated, ready to build, move to a directory other"
+echo "than the scripts to pull your source and build it."
+echo
+echo "i.e. => Run ./scripts/fai-get.sh from ../.. for example."
+echo
+echo "----------------------------------------------------------"
 
