6bc5f57d82ff60a5be5afaaf8cfb179190d3d028
W. Glen Boyd <glen@wgbdev.com>
2019-02-16 19:15:26 -0800

    removed quit

--- setup-4-fai.sh
+++ setup-4-fai.sh
@@ -1,4 +1,15 @@
+echo
+
 apt-get update 
 apt-get upgrade -y
 apt-get install devscripts 
 apt-get install git-buildpackage
+
+echo
+echo "-------------------------------------------------------"
+echo "NOTE:"
+echo "-------------------------------------------------------"
+echo "Now call ./setup-perl.sh to install the needed packages"
+echo
+
+
