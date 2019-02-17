2c9ff0f67297566a52aa584c0f7bff4f9e457340
W. Glen Boyd <glen@wgbdev.com>
2019-02-17 07:37:54 +0100

    refactor

--- fai/perl-setup.sh
+++ fai/perl-setup.sh
@@ -6,13 +6,23 @@ echo "-------------------------------------"
 echo "Installing Perl Packages...."
 echo "-------------------------------------"
 echo
+echo
+echo "Type the following commands into the terminal, "
+echo "using one per line."
+echo 
+cat perl-installs-1.txt
+echo
 
-#perl -MCPAN -e shell < perl-installs-1.txt
+perl -MCPAN -e shell 
 
 echo "Next --------------------------------"
 echo
+echo
+echo "Type the following commands, one per line."
+echo
+cat perl-installs-2.txt
 
-perl -MCPAN -e shell < perl-installs-2.txt
+perl -MCPAN -e shell
 
 #echo "perl -MCPAN -e shell"
 #echo
