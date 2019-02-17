1191c9e54a0f324f18c42cd99036d9b8fd32d17b
W. Glen Boyd <glen@wgbdev.com>
2019-02-17 08:21:06 +0100

    added perl dependencies

--- fai/setup-4-fai.sh
+++ fai/setup-4-fai.sh
@@ -5,6 +5,9 @@ apt-get upgrade -y
 apt-get install devscripts -y
 apt-get install git-buildpackage -y
 apt-get install libgraph-perl -y
+#apt-get install libparse-recdescent-perl -y
+apt-get install asciidoc -y
+apt-get install w3m -y
 
 echo
 echo "-------------------------------------------------------"
