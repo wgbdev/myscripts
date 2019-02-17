74cd4b29fe0eec0568c6e52087594a0bb7d90585
W. Glen Boyd <glen@wgbdev.com>
2019-02-17 08:29:29 +0100

    get perl working...

--- fai/setup-4-fai.sh
+++ fai/setup-4-fai.sh
@@ -5,9 +5,11 @@ apt-get upgrade -y
 apt-get install devscripts -y
 apt-get install git-buildpackage -y
 apt-get install libgraph-perl -y
-#apt-get install libparse-recdescent-perl -y
+apt-get install libparse-recdescent-perl -y
 apt-get install asciidoc -y
 apt-get install w3m -y
+# - Let`s hope we don`t need the following, it's really fucking big....
+#apt-get install dblatex -y
 
 echo
 echo "-------------------------------------------------------"
